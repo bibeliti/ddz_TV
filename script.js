1be100b7ddfc82967aa4dafad560a2a019b7fbac
bibeliti <116756397+bibeliti@users.noreply.github.com>
2023-05-09 15:10:43 +0300

    add index

--- script.js
+++ script.js
@@ -8,6 +8,7 @@ for (i = 0; i < myNodelist.length; i++) {
     myNodelist[i].appendChild(span);
 }
 
+
 var close = document.getElementsByClassName("close");
 var i;
 for (i = 0; i < close.length; i++) {
