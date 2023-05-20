dc22367327371443abefd08a6d522bd4b8eaec34
bibeliti <116756397+bibeliti@users.noreply.github.com>
2023-05-20 19:14:02 +0300

    add new table for loss each event and strategy

--- script.js
+++ script.js
@@ -1,16 +1,51 @@
 function consoleTable() {
     let data = {}
-    data['base'] = document.getElementById("baseValue").textContent;
-    tableEntry("tableRiskAssessment", "risk", data)
+    data['base'] = Number(document.getElementById("baseValue").textContent)
+    console.log(data['base'])
+    tableEntry("tableEvent", "event", data)
     tableEntry("tableMinimization", "minimization", data)
-
+    printTableAltLoss(data)
     console.log(data)
 }
+
+function printTableAltLoss(data) {
+    let table = document.getElementById("tableBodyAltLoss")
+    let th = document.createElement('tr')
+    for (let i = 0; i < data['event'].length + 2; i++) {
+        let td = document.createElement("td")
+        if (i !== 0 && i !== data['event'].length + 1) {
+            let name = document.createTextNode(data["event"][i - 1][0])
+            td.appendChild(name)
+        }
+        th.appendChild(td)
+    }
+    table.appendChild(th);
+    for (let rows = 0; rows < data['minimization'].length; rows++) {
+        let tr = document.createElement("tr")
+        for (let cols = 0; cols < data['event'].length + 2; cols++) {
+            let text
+            let td = document.createElement("td")
+            if (cols === 0) {
+                text = document.createTextNode(data["minimization"][rows - 1])
+            } else if (cols === data['event'].length + 1) {
+                text = document.createTextNode("99999")
+            } else {
+                let lossMoneyNumber = data['base'] * data['event'][cols - 1][3]
+                lossMoneyNumber = lossMoneyNumber.toString()
+                text = document.createTextNode(lossMoneyNumber)
+            }
+            td.appendChild(text)
+            tr.appendChild(td)
+        }
+        table.appendChild(tr)
+    }
+}
+
 
 function tableEntry(nameTable, nameData, data) {
     let tableRisk = document.getElementById(nameTable)
 
-    data[nameData] = [];
+    data[nameData] = []
     let currRows = tableRisk.getElementsByTagName('tr')
 
     for (let j = 0; j < currRows.length; j++) {
@@ -18,93 +53,100 @@ function tableEntry(nameTable, nameData, data) {
         let cells = currRows[j].getElementsByTagName('td')
 
         for (let k = 0; k < cells.length - 1; k++) {
-            data[nameData][j][k] = cells[k].textContent
+            data[nameData][j][k] = Number(cells[k].textContent)
         }
     }
 }
 
 
-let myNodelist = document.getElementsByTagName("tr");
-let i;
-for (i = 0; i < myNodelist.length; i++) {
-    let span = document.createElement("span");
-    let txt = document.createTextNode("\u00D7");
-    span.className = "close";
-    span.appendChild(txt);
-    myNodelist[i].appendChild(span);
+let myNodelist = document.getElementsByTagName("tr")
+for (let i = 0; i < myNodelist.length; i++) {
+    let span = document.createElement("span")
+    let txt = document.createTextNode("\u00D7")
+    span.className = "close"
+    span.appendChild(txt)
+    myNodelist[i].appendChild(span)
 }
 
-let close = document.getElementsByClassName("close");
-for (i = 0; i < close.length; i++) {
+let close = document.getElementsByClassName("close")
+for (let i = 0; i < close.length; i++) {
     close[i].onclick = function () {
-        let div = this.parentElement;
-        div.style.display = "none";
+        let div = this.parentElement
+        div.style.display = "none"
     }
 }
 
-function newRisk() {
-    let tr = document.createElement("tr");
-    let inputRiskName = document.getElementById("inputRiskName").value;
-    let inputRiskAssessment = document.getElementById("inputRiskAssessment").value;
-    let inputRiskIntensity = document.getElementById("inputRiskIntensity").value;
-    let textName = document.createTextNode(inputRiskName);
-    let textAssessment = document.createTextNode(inputRiskAssessment);
-    let textIntensive = document.createTextNode(inputRiskIntensity);
+function newEvent() {
+    let tr = document.createElement("tr")
+    let inputEventName = document.getElementById("inputEventName").value
+    let inputEventIntensity = document.getElementById("inputEventIntensity").value
+    let inputEventProbability = document.getElementById("inputEventProbability").value
+    let inputEventOccurrence = document.getElementById("inputEventOccurrence").value
+    let textName = document.createTextNode(inputEventName)
+    let textIntensive = document.createTextNode(inputEventIntensity)
+    let textProbability = document.createTextNode(inputEventProbability)
+    let textOccurrence = document.createTextNode(inputEventOccurrence)
     let name = document.createElement("td")
     let intensive = document.createElement("td")
-    let assessment = document.createElement("td")
+    let probability = document.createElement("td")
+    let occurrence = document.createElement("td")
     name.appendChild(textName)
-    assessment.appendChild(textAssessment)
     intensive.appendChild(textIntensive)
+    probability.appendChild(textProbability)
+    occurrence.appendChild(textOccurrence)
     tr.appendChild(name)
     tr.appendChild(intensive)
-    tr.appendChild(assessment)
-    if (inputRiskAssessment === '' || inputRiskIntensity === '' || inputRiskName === '') {
-        alert("You must write something!");
+    tr.appendChild(probability)
+    tr.appendChild(occurrence)
+    if (inputEventName === '' || inputEventIntensity === '' || inputEventProbability === ''
+        || inputEventOccurrence === '') {
+        alert("You must write something!")
     } else {
-        if(!isNaN(inputRiskIntensity) && !isNaN(inputRiskAssessment) && (0 <= inputRiskAssessment) && (inputRiskAssessment <= 100)){
-        document.getElementById("tableRiskAssessment").appendChild(tr);
-        document.getElementById("tableRiskAssessment").removeAttribute("class");
-        }
-        else {
+        if (!isNaN(inputEventIntensity) && !isNaN(inputEventProbability) && !isNaN(inputEventOccurrence)
+            && (0 <= inputEventProbability) && (inputEventProbability <= 100) && (0 <= inputEventOccurrence)
+            && (inputEventOccurrence <= 100)) {
+            document.getElementById("tableEvent").appendChild(tr)
+            document.getElementById("tableEvent").removeAttribute("class")
+        } else {
             alert("Digit please!");
         }
     }
-    document.getElementById("inputRiskName").value = "";
-    document.getElementById("inputRiskAssessment").value = "";
-    document.getElementById("inputRiskIntensity").value = "";
-    createCloseButton(tr);
+    document.getElementById("inputEventName").value = ""
+    document.getElementById("inputEventIntensity").value = ""
+    document.getElementById("inputEventProbability").value = ""
+    document.getElementById("inputEventOccurrence").value = ""
+    createCloseButton(tr)
 }
 
 function createCloseButton(tr) {
-    let closeButton = document.createElement("td");
+    let closeButton = document.createElement("td")
     let spanForClose = document.createElement("span")
-    let txt = document.createTextNode("\u00D7");
+    let txt = document.createTextNode("\u00D7")
     spanForClose.appendChild(txt)
-    spanForClose.className = "close";
-    closeButton.appendChild(spanForClose);
+    spanForClose.className = "close"
+    closeButton.appendChild(spanForClose)
     closeButton.className = "lastTd"
-    tr.appendChild(closeButton);
+    tr.appendChild(closeButton)
 
-    for (i = 0; i < close.length; i++) {
+    for (let i = 0; i < close.length; i++) {
         close[i].onclick = function () {
-            let div = this.parentElement.parentElement;
-            div.setAttribute('class', 'deleteClass');
-            const deleteElement = document.querySelector('.deleteClass');
-            const parent = deleteElement.parentNode;
-            parent.removeChild(deleteElement);
+            let div = this.parentElement.parentElement
+            div.setAttribute('class', 'deleteClass')
+            const deleteElement = document.querySelector('.deleteClass')
+            const parent = deleteElement.parentNode
+            parent.removeChild(deleteElement)
         }
     }
 }
 
 function newMinimization() {
-    let tr = document.createElement("tr");
-    let inputNameMinimization = document.getElementById("inputNameMinimization").value;
-    let inputRiskStrategy = document.getElementById("inputWhatRiskThisStrategy").value;
-    let inputCostMinimization = document.getElementById("inputCostMinimization").value;
-    let textName = document.createTextNode(inputNameMinimization);
-    let textRiskStrategy = document.createTextNode(inputRiskStrategy);
-    let textCost = document.createTextNode(inputCostMinimization);
+    let tr = document.createElement("tr")
+    let inputNameMinimization = document.getElementById("inputNameMinimization").value
+    let inputRiskStrategy = document.getElementById("inputWhatRiskThisStrategy").value
+    let inputCostMinimization = document.getElementById("inputCostMinimization").value
+    let textName = document.createTextNode(inputNameMinimization)
+    let textRiskStrategy = document.createTextNode(inputRiskStrategy)
+    let textCost = document.createTextNode(inputCostMinimization)
     let name = document.createElement("td")
     let riskStrategy = document.createElement("td")
     let cost = document.createElement("td")
@@ -115,44 +157,52 @@ function newMinimization() {
     tr.appendChild(riskStrategy)
     tr.appendChild(cost)
     if (inputNameMinimization === '' || inputRiskStrategy === '' || inputCostMinimization === '') {
-        alert("You must write something!");
+        alert("You must write something!")
     } else {
-        if(!isNaN(inputCostMinimization) ){
-        document.getElementById("tableMinimization").appendChild(tr);
-        document.getElementById("tableMinimization").removeAttribute("class");
+        if (!isNaN(inputCostMinimization)) {
+            document.getElementById("tableMinimization").appendChild(tr);
+            document.getElementById("tableMinimization").removeAttribute("class");
         } else {
             alert("Digits please!");
         }
     }
-    document.getElementById("inputNameMinimization").value = "";
-    document.getElementById("inputWhatRiskThisStrategy").value = "";
-    document.getElementById("inputCostMinimization").value = "";
-    createCloseButton(tr);
+    document.getElementById("inputNameMinimization").value = ""
+    document.getElementById("inputWhatRiskThisStrategy").value = ""
+    document.getElementById("inputCostMinimization").value = ""
+    createCloseButton(tr)
 }
 
-// function newBase() {
-//     let inputBase = document.getElementById("inputBase").value;
-//     if (inputBase === '') {
-//         alert("You must write something!");
-//     } else {
-//     }
-//     document.getElementById("inputBase").value = "";
-// }
-
-
 function newBase() {
-    var inputBase = document.getElementById("inputBase").value;
-    var myRe = /\d*/g; // '' !!!!!!!!!
-    if(inputBase === '') {
+    let inputBase = document.getElementById("inputBase").value;
+    let myRe = /\d*/g; // '' !!!!!!!!!
+    if (inputBase === '') {
         alert("You must write something!");
     } else {
-        if(!isNaN(inputBase)){ // null inputBase.match(/'[0-9]*'/gi)
+        document.getElementById("baseValue").innerText = document.createTextNode(inputBase).nodeValue
+        document.getElementById("forBase").removeAttribute("class")
+        document.getElementById("addBtnBase").innerText = "Обновить"
+        if (!isNaN(inputBase)) { // null inputBase.match(/'[0-9]*'/gi)
             document.getElementById("baseValue").innerText = document.createTextNode(inputBase).nodeValue;
             document.getElementById("forBase").removeAttribute("class");
             document.getElementById("addBtnBase").innerText = "Обновить";
-        } else{
+        } else {
             alert("Digits please!");
         }
     }
-    document.getElementById("inputBase").value = "";
+    document.getElementById("inputBase").value = ""
+}
+
+function creatingTableWithLostProfits(data) {
+    const table = document.getElementById("tableLostProfits")
+    table.removeAttribute('class')
+    let th = document.createElement('th')
+    let tdColumnNumber = document.createElement('td')
+    let tdNameParam = document.createElement('td')
+    tdColumnNumber.appendChild(document.createTextNode("Number"))
+    tdNameParam.appendChild(document.createTextNode("Name param"))
+
+
+    th.appendChild(tdColumnNumber)
+
+    table.appendChild(th)
 }
