a1f3a32861c8d00204a3b7ef3108a76e9a95c1bd
bibeliti <116756397+bibeliti@users.noreply.github.com>
2023-06-23 23:45:31 +0300

    теперь еще работает стратегии

--- newScript.js
+++ newScript.js
@@ -1,5 +1,6 @@
 let data = {}
 data["countEvent"] = 0
+data["base"] = 0
 
 function main() {
     let data = {}
@@ -53,17 +54,17 @@ function printRenameEvents(data) {
     tdEvent.appendChild(textTdEvent);
     tdRename.appendChild(textTdRename);
 
-    data['event'] = new Array(data['userEvent'].length)
-    for (let i = 0; i < data['userEvent'].length; i++) {
+    data["event"] = new Array(data["userEvent"].length)
+    for (let i = 0; i < data["userEvent"].length; i++) {
         let tr = document.createElement("tr");
         let tdNameOriginal = document.createElement("td");
         let tdNameReplacement = document.createElement("td");
 
-        let original = document.createTextNode(data['userEvent'][i][0]);
-        let replacement = document.createTextNode('Событие-' + (i + 1));
+        let original = document.createTextNode(data["userEvent"][i][0]);
+        let replacement = document.createTextNode("Событие-" + (i + 1));
 
-        data['event'][i] = data['userEvent'][i]
-        data['event'][i][0] = 'e' + (i + 1)
+        data["event"][i] = data["userEvent"][i]
+        data["event"][i][0] = "e" + (i + 1)
 
         tdNameOriginal.appendChild(textTd);
         tdNameReplacement.appendChild(textTd2);
@@ -78,7 +79,7 @@ function newBase() {
     let inputRent = document.getElementById("validationRentValue").value
     let divForBase = document.getElementById("divForBase")
     let divForRent = document.getElementById("divForRent")
-    document.querySelectorAll('.invalid-feedback').forEach(e => e.remove())
+    document.querySelectorAll(".invalid-feedback").forEach(e => e.remove())
     document.getElementById("validationBaseValue").classList.remove("is-invalid")
     document.getElementById("validationRentValue").classList.remove("is-invalid")
     let flag = true
@@ -129,6 +130,8 @@ function newBase() {
         document.getElementById("valueOutput").setAttribute("class", "col-md-12 md-12 form-row")
         document.getElementById("pForBaseValue").innerText = inputBase
         document.getElementById("pForRentValue").innerText = inputRent
+        data["base"] = inputBase
+        data["rent"] = inputRent
     }
     document.getElementById("validationBaseValue").value = ""
     document.getElementById("validationRentValue").value = ""
@@ -140,7 +143,7 @@ function newEvent() {
     let probabilityEvent = document.getElementById("validationProbabilityEvent").value
     let divForIntensityEvent = document.getElementById("divForIntensityEvent")
     let divForProbabilityEvent = document.getElementById("divForProbabilityEvent")
-    document.querySelectorAll('.invalid-feedback').forEach(e => e.remove())
+    document.querySelectorAll(".invalid-feedback").forEach(e => e.remove())
     document.getElementById("validationIntensityEvent").classList.remove("is-invalid")
     document.getElementById("validationProbabilityEvent").classList.remove("is-invalid")
 
@@ -180,8 +183,8 @@ function newEvent() {
 
     if (flag) {
         let events = document.getElementById("Events")
-        let event = document.createElement('tr')
-        let newNameEvent = document.createElement('td')
+        let event = document.createElement("tr")
+        let newNameEvent = document.createElement("td")
         newNameEvent.setAttribute("class", "col-md-3 mb-3 eventName")
         newNameEvent.innerHTML = nameEvent
         let newIntensityEvent = document.createElement("td")
@@ -207,10 +210,8 @@ function newEvent() {
         document.getElementById("validationAtWhichEvent").appendChild(newOption)
 
         let close = document.getElementsByClassName("close")
-        console.log(close)
 
         for (let i = 0; i < close.length; i++) {
-            console.log(i)
             close[i].onclick = function () {
                 let div = this.parentElement.parentElement
                 div.setAttribute("class", "deleteClass")
@@ -239,12 +240,101 @@ function newEvent() {
 
 }
 
-
 function createCloseButton(div) {
     let spanForClose = document.createElement("span")
     let txt = document.createTextNode("\u00D7")
     spanForClose.appendChild(txt)
     spanForClose.className = "close"
     div.appendChild(spanForClose)
-    // let close = document.querySelectorAll(".close")
+}
+
+function newMinimization() {
+    let nameStrategy = document.getElementById("validationNameStrategy").value
+    let atWhichEvent = document.getElementById("validationAtWhichEvent").value
+    let costStrategy = document.getElementById("validationCostsStrategy").value
+    let howMach = document.getElementById("validationByHowMuch").value
+    let divCostStrategy = document.getElementById("divForCostStrategy")
+    let divHowMach = document.getElementById("divForHowMach")
+    document.querySelectorAll(".invalid-feedback").forEach(e => e.remove())
+    document.getElementById("validationAtWhichEvent").classList.remove("is-invalid")
+    document.getElementById("validationCostsStrategy").classList.remove("is-invalid")
+
+    let flag = true
+    if ((isNaN(costStrategy)) || (Number(costStrategy) < 0)) {
+        flag = false
+        document.getElementById("validationCostsStrategy").setAttribute("class", "form-control is-invalid")
+        let feedbackCost = document.createElement("div")
+        feedbackCost.setAttribute("class", "invalid-feedback")
+        feedbackCost.innerHTML = "Введите положительное число"
+        divCostStrategy.appendChild(feedbackCost)
+    } else if (Number(costStrategy) > data["base"]) {
+        flag = false
+        document.getElementById("validationCostsStrategy").setAttribute("class", "form-control is-invalid")
+        let feedbackCost = document.createElement("div")
+        feedbackCost.setAttribute("class", "invalid-feedback")
+        feedbackCost.innerHTML = "Стоимость стратегии не может быть больше базы"
+        divCostStrategy.appendChild(feedbackCost)
+    } else {
+        costStrategy = Number(costStrategy)
+    }
+    if ((isNaN(howMach)) || (Number(howMach) < 0) || (Number(howMach) > 1)) {
+        flag = false
+        document.getElementById("validationProbabilityEvent").setAttribute("class", "form-control is-invalid")
+        let feedbackHowMach = document.createElement("div")
+        feedbackHowMach.setAttribute("class", "invalid-feedback")
+        feedbackHowMach.innerHTML = "Введите положительное дробное число меньшее 1"
+        divHowMach.appendChild(feedbackHowMach)
+    } else {
+        howMach = Number(howMach)
+    }
+    if (flag) {
+        let strategies = document.getElementById("Strategies")
+        let strategy = document.createElement("tr")
+        let newNameStrategy = document.createElement("td")
+        newNameStrategy.setAttribute("class", "col-md-3 mb-3")
+        newNameStrategy.innerHTML = nameStrategy
+        let newAtWhichEvent = document.createElement("td")
+        newAtWhichEvent.setAttribute("class", "col-md-3 mb-3")
+        newAtWhichEvent.innerHTML = atWhichEvent
+        let newCostStrategy = document.createElement("td")
+        newCostStrategy.setAttribute("class", "col-md-3 mb-3")
+        newCostStrategy.innerHTML = costStrategy
+        let newHowMAch = document.createElement("td")
+        newHowMAch.setAttribute("class", "col-md-2 mb-2")
+        newHowMAch.innerHTML = howMach
+        let newDivClose = document.createElement("td")
+        createCloseButton(newDivClose)
+
+        strategy.appendChild(newNameStrategy)
+        strategy.appendChild(newAtWhichEvent)
+        strategy.appendChild(newCostStrategy)
+        strategy.appendChild(newHowMAch)
+        strategy.appendChild(newDivClose)
+        strategy.classList.remove("displayNone")
+        strategies.appendChild(strategy)
+        let close = document.getElementsByClassName("close")
+
+        for (let i = 0; i < close.length; i++) {
+            close[i].onclick = function () {
+                let div = this.parentElement.parentElement
+                div.setAttribute("class", "deleteClass")
+                const deleteElement = document.querySelector(".deleteClass")
+                const parent = deleteElement.parentNode
+                parent.removeChild(deleteElement)
+                let strategyNames = document.getElementsByClassName("eventName")
+                let selector = document.getElementById("validationAtWhichEvent")
+                selector.innerHTML = ''
+                for (let i = 0; i < strategyNames.length; i++) {
+                    let option = document.createElement("option")
+                    option.innerHTML = strategyNames[i].textContent
+                    selector.appendChild(option)
+                }
+            }
+        }
+    }
+
+    document.getElementById("validationNameStrategy").value = ""
+    document.getElementById("validationAtWhichEvent").value = ""
+    document.getElementById("validationCostsStrategy").value = ""
+    document.getElementById("validationByHowMuch").value = ""
 }
